10bdd342b93d9e52eaf48ed45e4e87b71e2c8f38
benja4510 <benjamindiaz4518@gmail.com>
2026-04-16 10:12:12 -0300

    clsnodo

---
+++ PryEdDiazB/clsNodo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryEdDiazB
+{
+    internal class clsNodo
+    {
+        //Campos del nodo
+        private Int32 cod;
+        private String nom;
+        private String tra;
+        private clsNodo Sig;
+
+        // Propiedades del nodo
+        public Int32 Codigo
+        {
+            get { return cod; }
+            set { cod = value; }
+        }
+
+        public String Nombre
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public String Tramite
+        {
+            get { return tra; }
+            set { tra = value; }
+        }
+
+        public clsNodo Siguiente
+        {
+            get { return Sig; }
+            set { Sig = value; }
+        }
+    }   }
